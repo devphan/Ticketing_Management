e3ca3dbf291ef9ab0ee57e7b5ab0193ee0812f43
Dev Phan <longphan12062001@gmail.com>
2022-03-19 21:27:52 +0700

    Build base project

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const rootRouter = require('./app/routers');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+//import router
+app.use(rootRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const error = {
+    error: 404,
+    message: 'not_found',
+    status: 400,
+    data: null
+  };
+  next(error);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  return res.json({
+    error: err.error,
+    message: err.message,
+    status: 400,
+    data: null
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
