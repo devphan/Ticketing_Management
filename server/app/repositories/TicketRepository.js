0df26f6e1afbb5418e7d3ff6ff3dc7935ba8cd68
Dev Phan <longphan12062001@gmail.com>
2022-04-02 09:56:17 +0700

    fix get all ticket by user

--- server/app/repositories/TicketRepository.js
+++ server/app/repositories/TicketRepository.js
@@ -1,4 +1,4 @@
-const { Ticket } = require('../models');
+const { Ticket, Trip, User, Station } = require('../models');
 
 
 class TicketRepository {
@@ -7,7 +7,30 @@ class TicketRepository {
     };
 
     async getAllTicketOfUser(userId) {
-        return await Ticket.findAll({ where: { userId } });
+        return await Ticket.findAll({
+            include: [
+                {
+                    model: Trip,
+                    attributes: { exclude: ['password'] },
+                    include: [
+                        {
+                            model: Station,
+                            as: 'from'
+                        },
+                        {
+                            model: Station,
+                            as: 'to'
+                        }
+                    ]
+                },
+                {
+                    model: User,
+                    attributes: { exclude: ['password'] }
+                }
+
+            ],
+            where: { userId }
+        });
     }
 
 }
